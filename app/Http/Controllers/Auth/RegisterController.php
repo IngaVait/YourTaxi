f034dc4297cd9019a1a63f2ef06ec78900adacf5
IngaVait <inga.vaitiekuniene@gmail.com>
2020-02-12 19:38:59 +0200

    papildyta registracija: pridetas phone ir surname, validuoja kaip reikia

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,9 +50,12 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:40'],
+            'surname' => ['required', 'string', 'max:40'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'phone' => ['integer', 'min:9'],
+
         ]);
     }
 
@@ -68,6 +71,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'phone' => $data['phone'],
+            'surname' => $data['surname']
         ]);
     }
 }
